47516e4768a3fb35af30ba5f84a555e98644e733
kiko.devv <Kiko.devv@gmail.com>
2025-12-04 06:16:06 +0100

    Added the source file for templates

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,30 @@
 import { BackgroundLines } from "@/components/ui/background-lines";
 import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 import { VideoText } from "@/components/ui/video-text";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="mb-600">
       <BackgroundLines>
         <ContainerScroll
           titleComponent={
             <div className="relative h-30 w-full overflow-hidden">
               <VideoText
                 fontSize={12}
-                src="https://videos.pexels.com/video-files/1918465/1918465-uhd_2560_1440_24fps.mp4">
+                src="/text-video.mp4">
                 VÄLKOMMEN
               </VideoText>
             </div>
           }>
-          {" "}
+          <Image
+            src="/title.jpg"
+            alt="Hero image"
+            unoptimized
+            height={720}
+            width={1280}
+            className="w-full h-full object-cover rounded-xl"
+          />
         </ContainerScroll>
       </BackgroundLines>
     </div>
