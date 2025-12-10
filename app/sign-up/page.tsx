be0feb701798975160ef421ba2c8f82a578a6919
kiko.devv <Kiko.devv@gmail.com>
2025-12-10 04:14:02 +0100

    Added sign up backend

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,17 +1,56 @@
 "use client";
-
 import Link from "next/link";
 import { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/lib/firebase/firebaseConfig";
+
 export default function SignUp() {
   /*--------- state for registeration ----------*/
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordMatch, setPasswordMatch] = useState("");
-
+  const [errorM, setErrorM] = useState("");
+  const [succecMassage, setSuccecMassage] = useState("");
+  const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
   /*--------- register logic ----------*/
-  const handleRegister = (e: React.FormEvent) => {
+  const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
     console.log(email, password, passwordMatch);
+
+    /*--------- input must not be empty ----------*/
+    if (!password || !email || !passwordMatch) {
+      console.log("Password is empty");
+      setErrorM("Alla fält måste fyllas");
+      return;
+    }
+
+    /*--------- password must be longer than 6 ----------*/
+    if (password.length < 6) {
+      setErrorM("lösenord måste vara minst 6st");
+      setPassword("");
+      setPasswordMatch("");
+      return;
+    }
+    if (password === passwordMatch) {
+      console.log("password match!");
+      setErrorM("");
+      try {
+        const res = await createUserWithEmailAndPassword(email, password);
+        console.log(res);
+        setEmail("");
+        setPassword("");
+        setPasswordMatch("");
+        setSuccecMassage("Du är registrerad nu");
+      } catch (e) {
+        console.log(e);
+        setErrorM("Ett fel uppstod vid registrering");
+      }
+    } else {
+      console.log("passwords don't match");
+      setErrorM("Lösenord måste vara lika");
+      setPassword("");
+      setPasswordMatch("");
+    }
   };
 
   return (
@@ -43,6 +82,8 @@ export default function SignUp() {
             className="bg-gray-200 p-2 rounded-md"
             placeholder="Bekräfta lösenordet"
           />
+          <p className="text-md text-red-500">{errorM}</p>
+          <p className="text-md text-green-500">{succecMassage}</p>
           <button
             type="submit"
             className="px-4 py-1 rounded-md cursor-pointer hover:bg-neutral-500 bg-neutral-600 text-white w-fit justify-self-center mt-10">
